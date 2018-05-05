7c387c44b5774c4648215d0b338afed45ff1a018
Duncan <marjoribanks.d@gmail.com>
2018-05-05 12:01:44 +0100

    added save functions to team and game classes

--- models/game.rb
+++ models/game.rb
@@ -10,6 +10,15 @@ class Game
     @winner = options['winner']
   end
 
-
+  def save()
+    sql = "INSERT INTO games
+    (team_a, team_b, winner)
+    VALUES
+    ($1, $2, $3)
+    RETURNING id"
+    values = [@team_a, @team_b, @winner]
+    result = SqlRunner.run(sql, values)
+    @id = result.first()['id'].to_i
+  end
 
 end

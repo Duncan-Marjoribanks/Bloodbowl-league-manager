e9cd111adcab35aafed21d0b5921a36ac7062729
Duncan <marjoribanks.d@gmail.com>
2018-05-07 08:57:57 +0100

    added game index table to display the id of all participants in all games

--- controllers/games_controller.rb
+++ controllers/games_controller.rb
@@ -1,3 +1,14 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/game')
+
+
+get '/games' do
+  @games = Game.all
+  erb ( :"games/index" )
+end
+
+# get '/games/:id' do
+#   @games = Game.find(params['id'].to_i)
+#   erb(:"games/show")
+# end
--- controllers/teams_controller.rb
+++ controllers/teams_controller.rb
@@ -1,3 +1,10 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/team')
+
+
+
+get '/teams' do
+  @team = Team.all
+  erb ( :"teams/index" )
+end
